bd5bc2c5a36130147a5e45a5408f4eb0024f9147
Drew Dabbs <dabbs.drew@gmail.com>
2021-10-23 01:06:40 -0400

    Initial commit'

---
+++ 01Project_KomodoCafe_ClassList/KomodoCafeMenuItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Project_KomodoCafe_ClassList
+{
+    //A meal number, so customers can say "I'll have the #5"
+    //A meal name
+    //A description
+    //A list of ingredients,
+    //A price
+    public class KomodoCafeMenuItem
+    {
+        public int MealNum { get; set; }
+        public string MealName { get; set; }
+        public string MealDescription { get; set; }
+        public string Ingredients { get; set; }
+        public double Price { get; set; }
+        public KomodoCafeMenuItem() { }
+        public KomodoCafeMenuItem(int mealNum, string mealName, string mealDescription, double price)
+        {
+            MealNum = mealNum;
+            MealName = mealName;
+            MealDescription = mealDescription;
+            Price = price;
+        }
+    }
+}
---
+++ KomodoCafe_Repository/KomodoCafe_Repo.cs
@@ -0,0 +1,15 @@
+﻿using _01Project_KomodoCafe_ClassList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoCafe_Repository
+{
+    public class KomodoCafe_Repo
+    {
+        private Dictionary<int, KomodoCafeMenuItem> _menu = new Dictionary<int, KomodoCafeMenuItem>();
+    }
+
+}
